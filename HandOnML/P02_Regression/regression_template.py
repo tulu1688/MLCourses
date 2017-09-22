3ec5aa8795d980d5259b2a6d93837dae92319b2b
Tran Khai <khai.tran@ascendcorp.com>
2017-09-22 15:07:53 +0700

    [Python] Add regression template

---
+++ HandOnML/P02_Regression/regression_template.py
@@ -0,0 +1,49 @@
+# Data Preprocessing Template
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('data.csv')
+# X must be matrix not a vector. So we need to specify the range of collumn [1:2]
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+# Splitting the dataset into the Training set and Test set
+"""from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
+
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)"""
+
+# Fitting the regression to the dataset
+# Create the regressor
+
+# Predicting a new result with regressor
+y_pred = regressor.predict(6.5)
+
+# Visualising the Regression Result
+plt.scatter(X,y,color='red')
+plt.plot(X,regressor.predict(X),color='blue')
+plt.title('Title')
+plt.xlabel('XLabel')
+plt.ylabel('YLabel')
+plt.show()
+
+# Visualising the Regression Result for higher resolution and a smoother curve
+X_grid = np.arange(min(X),max(X),0.1)
+X_grid = X_grid.reshape((len(X_grid),1))
+plt.scatter(X,y,color='red')
+plt.plot(X_grid,regressor.predict(X_grid),color='blue')
+plt.title('Title')
+plt.xlabel('XLabel')
+plt.ylabel('YLabel')
+plt.show()
